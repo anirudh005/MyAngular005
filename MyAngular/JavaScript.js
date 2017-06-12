f85ebfb713f9035a583086e3aa8e81cf19794000
anirudh005 <anirudh.v.1992@gmail.com>
2017-06-12 19:58:52 +0530

    WEB API TUT

---
+++ MyAngular/JavaScript.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="C:\Users\Anirudh\Source\Repos\MyAngular\MyAngular\MyAngular\angular.js" />
+var app = angular.module("Mymodule", [])
+.controller("Mycontroller", function ($scope) {
+    $scope.message = "this is from ngulaar ontroler"
+    $scope.employees =
+        [
+        {
+            "name": "Max",
+            "age": "32",
+            "occ": "Dentist"
+        },
+        {
+            "name": "Jim",
+            "age": "12",
+            "occ": "Student"
+        }
+        ]
+});
